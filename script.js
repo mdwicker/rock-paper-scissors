67fc5b3d92ea2102f11ebc3228ea25662c9a0f34
Matthew Wicker <mdwicker@gmail.com>
2020-12-24 09:39:16 -0800

    Report round results, prevent infinite gameplay

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 let computerScore = 0;
 let playerScore = 0;
 
+// global variable to check if the game is paused
+let gamePaused = false;
+
 // div elements to toggle game vs endgame page
-const divGame = document.getElementById('game');
+const divScore = document.getElementById('overall-score');
 const endGame = document.getElementById('endgame');
 
 
@@ -18,7 +21,7 @@ buttons.forEach(button => {
 const newGameButton = document.getElementById('new-game');
 newGameButton.addEventListener('click', newGame);
 
-// when "You" is clicked, toggle lizard-spock mode
+// when "You" is clicked, toggle lizard-Spock mode
 let lizardSpock = false;
 const lizardSpockMode = document.getElementById('lizard-spock');
 const lizardSpockItems = document.querySelectorAll('.lizard-spock');
@@ -26,23 +29,28 @@ lizardSpockMode.addEventListener('click', toggleLizardSpockMode);
 
 // play one round of Rock, Paper, Scissors
 function playRound(e) {
+    if (gamePaused) {
+        return;
+    }
+
     let computerSelection = computerPlay();
     let playerSelection = this.id;
 
     document.getElementById('round-choices').textContent = `You chose ${playerSelection}, the computer chose ${computerSelection}.`;
 
     winner = checkRoundWinner(computerSelection, playerSelection);
+    fightReport = getFightReport(computerSelection, playerSelection);
 
     // announce round winner
     if (winner === 'tie') {
         document.getElementById('round-results').textContent = 'Nothing happens.';
     }
     else if (winner === 'computer') {
-        document.getElementById('round-results').textContent = 'Sorry, you lose this round.';
+        document.getElementById('round-results').textContent = `${fightReport} Sorry, you lose this round.`;
         computerScore++;
     }
     else if (winner === 'player') {
-        document.getElementById('round-results').textContent = 'Way to go, you win this round!';
+        document.getElementById('round-results').textContent = `${fightReport} You win this round!`;
         playerScore++;
     }
     else {
@@ -53,17 +61,17 @@ function playRound(e) {
     document.getElementById('computer-score').textContent = computerScore;
 
     // check for the end of the game
-    if (playerScore === 3) {
+    if (playerScore >= 3) {
         gameOver('player');
     }
-    else if (computerScore === 3) {
+    else if (computerScore >= 3) {
         gameOver('computer');
     }
 }
 
 // return the computer's selection
 function computerPlay() {
-    // get a random number from 0 to 2, unless it's lizard spock mode
+    // get a random number from 0 to 2, unless it's lizard Spock mode
     let randomNum;
     if (!lizardSpock) {
         randomNum = Math.floor(Math.random() * 3);
@@ -72,7 +80,7 @@ function computerPlay() {
         randomNum = Math.floor(Math.random() * 5);
     }
 
-    // return either 'rock', 'paper', or 'scissors' (or lizard or spock), depending on the number
+    // return either 'rock', 'paper', or 'scissors' (or lizard or Spock), depending on the number
     switch (randomNum) {
         case 0:
             return 'rock';
@@ -87,7 +95,7 @@ function computerPlay() {
             return 'lizard';
         
         case 4:
-            return 'spock';
+            return 'Spock';
     }
 }
 
@@ -106,9 +114,9 @@ function checkRoundWinner(computerSelection, playerSelection) {
             }
             return 'player';
 
-        // if the computer chose paper, they beat rock and spock
+        // if the computer chose paper, they beat rock and Spock
         case 'paper':
-            if ( (playerSelection === 'rock') || (playerSelection === 'spock') ) {
+            if ( (playerSelection === 'rock') || (playerSelection === 'Spock') ) {
                 return 'computer';
             }
             return 'player';
@@ -120,15 +128,15 @@ function checkRoundWinner(computerSelection, playerSelection) {
             }
             return 'player';
         
-        // if the computer chose lizard, they beat spock and paper
+        // if the computer chose lizard, they beat Spock and paper
         case 'lizard':
-            if ( (playerSelection === 'spock') || (playerSelection === 'paper') ) {
+            if ( (playerSelection === 'Spock') || (playerSelection === 'paper') ) {
                 return 'computer';
             }
             return 'player';
         
-        // if the computer chose spock, they beat rock and scissors
-        case 'spock':
+        // if the computer chose Spock, they beat rock and scissors
+        case 'Spock':
             if ( (playerSelection === 'rock') || (playerSelection === 'scissors') ) {
                 return 'computer';
             }
@@ -136,15 +144,39 @@ function checkRoundWinner(computerSelection, playerSelection) {
     }
 }
 
+function getFightReport(firstSelection, secondSelection) {
+    let rock = (firstSelection === 'rock' || secondSelection === 'rock');
+    let paper = (firstSelection === 'paper' || secondSelection === 'paper');
+    let scissors = (firstSelection === 'scissors' || secondSelection === 'scissors');
+    let lizard = (firstSelection === 'lizard' || secondSelection === 'lizard');
+    let Spock = (firstSelection === 'Spock' || secondSelection === 'Spock');
+
+    switch (true) {
+        case (scissors && lizard): return 'Scissors decapitates lizard.';
+        case (scissors && paper): return 'Scissors cuts paper.';
+        case (paper && rock): return 'Paper covers rock.';
+        case (rock && lizard): return 'Rock crushes lizard.';
+        case (lizard && Spock): return 'Lizard poisons Spock.';
+        case (Spock && scissors): return 'Spock smashes scissors.';
+        case (scissors && lizard): return 'Scissors decapitates lizard.';
+        case (lizard && paper): return 'Lizard eats paper.';
+        case (paper && Spock): return 'Paper disproves Spock.';
+        case (Spock && rock): return 'Spock vaporizes rock.';
+        case (rock && scissors): return 'Rock crushes scissors.';
+    }
+}
+
 function gameOver(winner) {
+    gamePaused = true;
+
     if (winner === 'player') {
-        endGame.textContent = `You beat the computer ${playerScore} to ${computerScore}!`;
+        endGame.textContent = `Game over.\r\n You beat the computer ${playerScore} to ${computerScore}!`;
     }
     else if (winner === 'computer') {
-        endGame.textContent = `Whoops, you lost ${playerScore} to ${computerScore}.`;
+        endGame.textContent = `Game over.\r\nYou lost ${playerScore} to ${computerScore}.`;
     }
     
-    divGame.classList.toggle('invisible');
+    divScore.classList.toggle('invisible');
     endGame.classList.toggle('invisible');
 }
 
@@ -155,8 +187,9 @@ function newGame() {
     document.getElementById('round-results').textContent = 'First to 3 wins.';
     document.getElementById('player-score').textContent = playerScore;
     document.getElementById('computer-score').textContent = computerScore;
-    divGame.classList.remove('invisible');
+    divScore.classList.remove('invisible');
     endGame.classList.add('invisible');
+    gamePaused = false;
 }
 
 function toggleLizardSpockMode() {
